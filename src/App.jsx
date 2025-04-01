b3ffd927bad8ee4ac5810fd2598f692d514e3954
Nishansha Sachith Dilushan <151370609+NishanshaSachith@users.noreply.github.com>
2025-04-02 04:10:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Dashboard from "./components/Dashboard/Dashboard"; // Import your Dashboard component
+import { ThemeProvider } from "./components/ThemeContext/ThemeContext";
 
 const App = () => {
   return (
+    <ThemeProvider>
     <Router>
       <Dashboard />
     </Router>
+    </ThemeProvider>
   );
 };
 
